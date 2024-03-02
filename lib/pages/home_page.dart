9aa5e259e964011cf1039cb0c8bb26a983af950f
lightlitebug <light.lite.bug@gmail.com>
2024-03-02 23:46:44 +0900

    SearchPage widget

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../providers/weather/weather_provider.dart';
+import 'search_page.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -8,11 +12,34 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  String? _city;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Weather'),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.search),
+            onPressed: () async {
+              // to avoid async gap warning
+              // aka use_build_context_synchronously lint rule
+              final weatherContext = context.read<WeatherProvider>();
+
+              _city = await Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) {
+                  return const SearchPage();
+                }),
+              );
+              print('city: $_city');
+              if (_city != null) {
+                weatherContext.fetchWeather(_city!);
+              }
+            },
+          ),
+        ],
       ),
       body: const Center(
         child: Text('Home'),
